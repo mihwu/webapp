45147ee2541c3a7d44af98de9ff9206b94e3ff82
1620679429@qq.com <1620679429@qq.com>
2019-08-10 16:33:06 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+
+    
+    <br>
+    <f/>
+    
+  </div>
+</template>
+
+<script>
+import login from "./component/login.vue"
+export default {
+  data () {
+    return {
+      msg: 'Hello Vue!'
+    }
+  },
+  components:{'f':login}
+}
+</script>
+
+<style>
+body {
+  font-family:  Helvetica, sans-serif;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import login from '@/component/login.vue'
+
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: login
+    }
+  ]
+})
